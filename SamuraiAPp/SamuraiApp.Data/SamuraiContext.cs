bf48d670ba0c4bdce8ba43819aefd588b49fff51
Efra <efrain_serna@hotmail.com>
2022-05-06 17:40:29 -0500

    Adding, Getting entities.

--- SamuraiAPp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiAPp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
+using System;
 
 namespace SamuraiApp.Data
 {
@@ -11,7 +13,12 @@ namespace SamuraiApp.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData")
+                .LogTo(
+                Console.WriteLine,
+                new[] {DbLoggerCategory.Database.Command.Name},
+                LogLevel.Information)
+                .EnableSensitiveDataLogging();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- SamuraiAPp/SamuraiApp.UI/Program.cs
+++ SamuraiAPp/SamuraiApp.UI/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
 
 namespace SamuraiApp.UI
 {
     internal class Program
     {
+        private static SamuraiContext _context = new SamuraiContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //AddSamurais("Julie3", "Sampson3");
+            AddVariousTypes();
+            GetSamurais();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        static void AddSamurais(params string[] args)
+        {
+            foreach (string arg in args)
+            {
+                _context.Samurais.Add(new Domain.Samurai { Name = arg });
+            }
+            _context.SaveChanges();
+        }
+
+        private static void AddVariousTypes()
+        {
+            _context.Samurais.AddRange(
+                new Samurai { Name = "Shimada" },
+                new Samurai { Name = "Okamoto" }
+                );
+            _context.Battles.AddRange(
+                new Battle { Name = "Battle of Anegawa" },
+                new Battle { Name = "Battle of Nagashino" }
+                );
+            _context.SaveChanges();
+
+            _context.AddRange(
+                new Samurai { Name = "Shimada2" },
+                new Samurai { Name = "Okamoto2" },
+                new Battle { Name = "Battle of Anegawa 2" },
+                new Battle { Name = "Battle of Nagashino 2" }
+                );
+            _context.SaveChanges();
+        }
+
+        static void GetSamurais()
+        {
+            var result = _context.Samurais;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
     }
 }
